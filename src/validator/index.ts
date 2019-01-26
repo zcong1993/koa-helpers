f8c77b4fa4d401ace7b604c1bd21aa0c449a1ca1
zcong1993 <1005997919@qq.com>
2019-01-26 22:56:02 +0800

    add validator

---
+++ src/validator/index.ts
@@ -0,0 +1,19 @@
+import { validate as joiValidate, SchemaLike, ValidationError } from 'joi'
+import { invalidInputApiError } from '../error'
+
+export const validate = async (
+  data: any,
+  schema: SchemaLike,
+  exposeDetail: boolean = true
+) => {
+  try {
+    await joiValidate(data, schema)
+  } catch (err) {
+    if (exposeDetail)
+      throw invalidInputApiError(
+        (err as ValidationError).details.map(e => e.message)
+      )
+
+    throw invalidInputApiError([])
+  }
+}
