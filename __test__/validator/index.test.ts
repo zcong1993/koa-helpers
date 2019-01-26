0f7c98ed306385f4a3bac43fcef91bd33c0364b9
zcong1993 <1005997919@qq.com>
2019-01-27 00:17:18 +0800

    add validator test

---
+++ __test__/validator/index.test.ts
@@ -0,0 +1,28 @@
+import { validate, ApiError } from '../../src'
+import { SchemaLike, object, string } from 'joi'
+
+it('validate should works well', async () => {
+  const schema: SchemaLike = object().keys({
+    name: string()
+      .alphanum()
+      .min(6)
+      .required()
+  })
+
+  const testData = {
+    name: 'test'
+  }
+
+  await expect(validate(testData, schema)).rejects.toThrow(
+    new ApiError(
+      'INVALID_INPUT',
+      'INVALID_INPUT',
+      ['"name" length must be at least 6 characters long'],
+      400
+    )
+  )
+
+  await expect(validate(testData, schema, false)).rejects.toThrow(
+    new ApiError('INVALID_INPUT', 'INVALID_INPUT', [], 400)
+  )
+})
