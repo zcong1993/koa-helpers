7ee69bfc64ca8beea9bb8a2885fe2c6ad4620e26
zcong1993 <1005997919@qq.com>
2019-01-27 16:51:14 +0800

    update docs

--- src/error/index.ts
+++ src/error/index.ts
@@ -1,10 +1,20 @@
 import * as Koa from 'koa'
 
+/**
+ * ApiError is class base on Error class, includes `statusCode`, `code`, `message` and `messages`.
+ */
 export class ApiError extends Error {
   public code: string
   public statusCode: number
   public messages: string[]
 
+  /**
+   * ApiError constructor
+   * @param message Error detail message
+   * @param code Error code
+   * @param statusCode Error response status code
+   * @param messages Error details messages
+   */
   constructor(
     message: string = 'INTERNAL_ERROR',
     code: string = 'INTERNAL_ERROR',
@@ -18,9 +28,16 @@ export class ApiError extends Error {
   }
 }
 
+/**
+ * invalidInputApiError is helper function to create invalid input ApiError
+ * @param messages Error details messages
+ */
 export const invalidInputApiError = (messages: string[]): ApiError =>
   new ApiError('INVALID_INPUT', 'INVALID_INPUT', 400, messages)
 
+/**
+ * unauthorizedApiError is default unauthorizedApiError
+ */
 export const unauthorizedApiError = new ApiError(
   'UNAUTHORIZED_ERROR',
   'UNAUTHORIZED_ERROR',
@@ -28,12 +45,20 @@ export const unauthorizedApiError = new ApiError(
   []
 )
 
+/**
+ * notFoundApiError is helper function to create not found ApiError
+ * @param message Error detail message
+ * @param code Error code
+ */
 export const notFoundApiError = (
   message: string = 'NOT_FOUND',
   code: string = 'NOT_FOUND'
 ): ApiError => new ApiError(message, code, 400, [])
 
 /* istanbul ignore next */
+/**
+ * simpleErrorHandler create a koa middleware to handle ApiError globally
+ */
 export const simpleErrorHandler = (): Koa.Middleware => async (ctx, next) => {
   try {
     await next()
--- src/password/index.ts
+++ src/password/index.ts
@@ -1,19 +1,42 @@
 import * as bcrypt from 'bcrypt'
 
+/**
+ * SALT_ROUND is defalut salt round
+ */
 const SALT_ROUND = 10
 
+/**
+ * bcrypt hash method
+ * @param data data to be hashed
+ * @param saltOrRounds hash options, salt string or salt round
+ */
 export const hash = (
   data: any,
   saltOrRounds: string | number = SALT_ROUND
 ): Promise<string> => bcrypt.hash(data, saltOrRounds)
 
+/**
+ * bcrypt compare method
+ * @param password password string
+ * @param hash password hashed
+ */
 export const compare = (password: string, hash: string): Promise<boolean> =>
   bcrypt.compare(password, hash)
 
+/**
+ * bcrypt sync hash method
+ * @param data data to be hashed
+ * @param saltOrRounds hash options, salt string or salt round
+ */
 export const hashSync = (
   data: any,
   saltOrRounds: string | number = SALT_ROUND
 ): string => bcrypt.hashSync(data, saltOrRounds)
 
+/**
+ * bcrypt sync compare method
+ * @param password password string
+ * @param hash password hashed
+ */
 export const compareSync = (password: string, hash: string): boolean =>
   bcrypt.compareSync(password, hash)
